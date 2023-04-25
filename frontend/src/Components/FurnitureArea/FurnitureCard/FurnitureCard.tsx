321febe72b84bde5a3f198b2a7cddfd8b74b8e00
Michal Shenkar <0583294585m@gmail.com>
2023-04-26 00:54:21 +0300

    frontend - design and style furniture-card

--- frontend/src/Components/FurnitureArea/FurnitureCard/FurnitureCard.tsx
+++ frontend/src/Components/FurnitureArea/FurnitureCard/FurnitureCard.tsx
@@ -1,13 +1,17 @@
+import FurnitureModel from "../../../4-Models/FurnitureModel"
 import "./FurnitureCard.css"
 
 interface FurnitureCardProps{
-
+    furniture: FurnitureModel
 }
 
 function FurnitureCard(props: FurnitureCardProps): JSX.Element{
     return(
         <div className="FurnitureCard">
-
+            <h3>{props.furniture.furnitureTypeName} | {props.furniture.name}</h3>
+            <span>{props.furniture.description}</span> <br />
+            <span>{props.furniture.size} , {props.furniture.color}</span> <br />
+            <span> {props.furniture.price}$, {props.furniture.discount}% </span>
         </div>
     )
 }
