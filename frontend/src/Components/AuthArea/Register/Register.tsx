ae86442709d21d4240b661c0cd673b751813d8d4
Michal Shenkar <0583294585m@gmail.com>
2023-05-06 23:47:27 +0300

    add ReCaptcha

--- frontend/src/Components/AuthArea/Register/Register.tsx
+++ frontend/src/Components/AuthArea/Register/Register.tsx
@@ -3,8 +3,12 @@ import "./Register.css"
 import UserModel from "../../../4-Models/UserModel"
 import authService from "../../../3-Services/AuthService"
 import { useNavigate } from "react-router-dom"
+import ReCaptcha from 'react-google-recaptcha'
+import { useState } from "react"
 
 function Register(): JSX.Element {
+
+    const [isBot, setIsBot] = useState<boolean>(true)
 
     const {register, handleSubmit} = useForm<UserModel>()
     const navigate = useNavigate()
@@ -19,19 +23,28 @@ function Register(): JSX.Element {
         }
 
     }
+
+    function reCaptchaChecked(value: string): void {
+        setIsBot(value?.length === 0)
+    }
+
     return(
         <div className="Register">
             <form onSubmit={handleSubmit(send)}>
                 <label>First Name:</label>
-                <input type="text" {...register("firstName")} /> <br />
+                <input type="text" {...register("firstName")} required/> <br />
                 <label>Last Name:</label>
-                <input type="text" {...register("lastName")}/> <br />
+                <input type="text" {...register("lastName")} required/> <br />
                 <label>User Name:</label>
-                <input type="text" {...register("username")} /> <br />
+                <input type="text" {...register("username")} required /> <br />
                 <label>Password:</label>
-                <input type="password" {...register("password")}/>
+                <input type="password" {...register("password")} required/>
 
-                <button>Send</button>
+                <div className="ReCaptchContainer">
+                    <ReCaptcha sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" onChange={reCaptchaChecked} />
+                </div>
+
+                <button disabled={isBot}>Register</button>
             </form>
         </div>
     )
